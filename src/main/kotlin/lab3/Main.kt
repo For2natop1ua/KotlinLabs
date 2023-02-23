787208b8e89f541df57301566bb64f16f6346eaf
For2natop1ua <plisko2013@gmail.com>
2023-02-23 14:17:00 +0200

    lab 3

--- src/main/kotlin/lab3/Main.kt
+++ src/main/kotlin/lab3/Main.kt
@@ -3,6 +3,6 @@ package lab3
 fun main(){
     print("Вкажіть ваш вік: ")
     val age:Int = readln().toInt()
-    if (age >= 18) println("Користувач повнолітній")
-    else println("Користувач неповнолітній")
+    if (age >= 18) println("Користувач повнолітній.")
+    else println("Користувач неповнолітній.")
 }
