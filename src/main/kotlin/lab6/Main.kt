03f4cc66ef9cfe7c8bbb0a8c536c358359f61d6b
For2natop1ua <plisko2013@gmail.com>
2023-03-23 14:07:37 +0200

    lab 6

---
+++ src/main/kotlin/lab6/Main.kt
@@ -0,0 +1,25 @@
+package lab6
+
+/*Задано символьну матрицю розміром N x M.
+Визначити кількість різних особистих елементів матриці.
+(Тобто повторювані елементи рахувати один раз).*/
+
+fun main(){
+    val matrix = arrayOf(
+        arrayOf('a', 'b', 'c'),
+        arrayOf('d', 'e', 'f'),
+        arrayOf('a', 'b', 'g')
+    )
+    val distinctCount = countDistinctElements(matrix)
+    println("Кількість різних особистих елементів матриці: $distinctCount")
+}
+
+fun countDistinctElements(matrix: Array<Array<Char>>): Int {
+    val uniqueChars = mutableSetOf<Char>() // множина унікальних символів
+    for (row in matrix) {
+        for (element in row) {
+            uniqueChars.add(element) // додаємо кожний символ до множини
+        }
+    }
+    return uniqueChars.size // повертаємо кількість унікальних символів
+}
